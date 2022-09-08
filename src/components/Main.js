e3943a2b7f6b59c99b894d78a0f0b74360d10cac
Viacheslav Chernogorov <slavachernogorv@mail.ru>
2022-09-08 17:12:22 +0300

    feat: import api.js and utils.js from old project, use api for avatar and info

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,16 +1,34 @@
 import React from "react";
 import avatar from "../images/Profile__image.jpg";
 import profileBtn from "../images/Profile__Edit-Button.svg";
+import api from "../utils/api";
 
 function Main(props) {
-  
+  const [userName, setUserName] = React.useState("");
+  const [userDescription, setUserDescription] = React.useState("");
+  const [userAvatar, setUserAvatar] = React.useState("");
+
+  React.useEffect(() => {
+    api
+      .getUserInfoFromServer()
+      .then((info) => {
+        const userInfo = info;
+        setUserAvatar(userInfo.avatar);
+        setUserDescription(userInfo.about);
+        setUserName(userInfo.name);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [userAvatar, userDescription, userName]);
+
   return (
     <>
       <main className="main">
         <section className="profile">
           <div className="profile__avatar">
             <img
-              src={avatar}
+              src={userAvatar}
               alt="Фото профиля"
               className="profile__avatar-element"
             />
@@ -22,7 +40,7 @@ function Main(props) {
           </div>
 
           <div className="profile__info">
-            <h1 className="profile__title">Ататюрк М.К.</h1>
+            <h1 className="profile__title">{userName}</h1>
             <button
               type="button"
               className="profile__button"
@@ -30,7 +48,7 @@ function Main(props) {
             >
               <img src={profileBtn} alt="Кнопка редактирования" />
             </button>
-            <p className="profile__subtitle">Первый президент Турции</p>
+            <p className="profile__subtitle">{userDescription}</p>
           </div>
           <button
             type="button"
---
+++ src/utils/api.js
@@ -0,0 +1,90 @@
+class Api {
+  constructor({ url, headers }) {
+    this._url = url;
+    this._headers = headers;
+  }
+
+  _checkResponse(res) {
+    if (res.ok) {
+      return res.json();
+    }
+    return Promise.reject(`Что-то упало: ${res.status}`);
+  }
+
+  setUserAvatarToServer(data) {
+    return fetch(`${this._url}/users/me/avatar`, {
+      method: "PATCH",
+      headers: this._headers,
+      body: JSON.stringify({
+        avatar: data.avatar,
+      }),
+    }).then(this._checkResponse);
+  }
+
+  deleteLike(id) {
+    return fetch(`${this._url}/cards/${id}/likes`, {
+      method: "DELETE",
+      headers: this._headers,
+    }).then(this._checkResponse);
+  }
+
+  doLike(id) {
+    return fetch(`${this._url}/cards/${id}/likes`, {
+      method: "PUT",
+      headers: this._headers,
+    }).then(this._checkResponse);
+  }
+
+  deleteCard(id) {
+    return fetch(`${this._url}/cards/${id}`, {
+      method: "DELETE",
+      headers: this._headers,
+    }).then(this._checkResponse);
+  }
+
+  postCard(card) {
+    return fetch(`${this._url}/cards`, {
+      method: "POST",
+      headers: this._headers,
+      body: JSON.stringify(card),
+    }).then(this._checkResponse);
+  }
+
+  setUserInfoToServer(data) {
+    return fetch(`${this._url}/users/me`, {
+      method: "PATCH",
+      headers: this._headers,
+      body: JSON.stringify({
+        name: data.name,
+        about: data.about,
+      }),
+    }).then(this._checkResponse);
+  }
+
+  getCards() {
+    return fetch(`${this._url}/cards`, {
+      method: "GET",
+      headers: this._headers,
+    }).then(this._checkResponse);
+  }
+
+  getUserInfoFromServer() {
+    return fetch(`${this._url}/users/me`, {
+      headers: this._headers,
+    }).then(this._checkResponse);
+  }
+
+  getInitialCards() {
+    return Promise.all([this.getCards, this.getUserInfoFromServer]);
+  }
+}
+
+const api = new Api({
+  url: "https://mesto.nomoreparties.co/v1/cohort-48",
+  headers: {
+    authorization: "1b4bc79c-4655-4b7c-ab14-ef593343b332",
+    "Content-Type": "application/json",
+  },
+});
+
+export default api;
